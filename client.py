e3cd536a52a58084c6d558f7aeb497a502163f65
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-05-11 13:26:40 +0900

    add: 8 color

--- client.py
+++ client.py
@@ -12,17 +12,19 @@ fs = 250
 # ｎ分間のサンプル数を計算
 n_samples = fs * 60 * 5
 
-# サイン波の周波数と振幅を定義
+# サイン波の周波数を定義
 freq = 10  # 10 Hz
-amp = 1
+
+# 8つの振幅を指定
+amplitudes = [1, 2, 3, 4, 5, 6, 7, 8]
 
 # サイン波の波形を生成
 t = np.arange(n_samples) / fs
-waveform = amp * np.sin(2 * np.pi * freq * t)
+waveform = np.array([amp * np.sin(2 * np.pi * freq * t) for amp in amplitudes]).T
 
 # サンプルをストリームに送信
 for i in range(n_samples):
-    sample = [waveform[i % len(waveform)]] * 8
+    sample = waveform[i % len(waveform)]
     outlet.push_sample(sample)
     time.sleep(1/fs)
 
--- host.py
+++ host.py
@@ -17,10 +17,20 @@ class MainWindow(QMainWindow):
         # グラフを作成
         self.plot_widget = pg.PlotWidget()
         self.setCentralWidget(self.plot_widget)
-        self.plot_data_item = self.plot_widget.plot(pen='y')
+
+        # ウィンドウサイズを設定
+        self.setGeometry(100, 100, 800, 600)
 
         # グラフの初期値を設定
-        self.plot_data_item.setData(np.zeros(8))
+        self.plot_data_items = []
+        for i in range(8):
+            pen = pg.mkPen(color=(255 * i / 8, 0, 255 * (8 - i) / 8), width=2)
+            plot_data_item = self.plot_widget.plot(pen=pen)
+            plot_data_item.setData(np.zeros(10000) + i)
+            self.plot_data_items.append(plot_data_item)
+
+        # グラフの横軸範囲を設定
+        self.plot_widget.setXRange(0, 10000)
 
         # タイマーを設定して定期的にデータを受信する
         self.timer = QTimer(self)
@@ -30,7 +40,21 @@ class MainWindow(QMainWindow):
     def update_plot(self):
         # データを取得してプロット
         sample, timestamp = self.inlet.pull_sample()
-        self.plot_data_item.setData(np.hstack((self.plot_data_item.getData()[0][1:], sample)))
+        data = np.array([plot_data_item.getData()[1] for plot_data_item in self.plot_data_items])
+        data = np.array(data)
+        sample = np.array(sample)
+        data = np.hstack((data[:, 1:], sample.reshape(-1, 1)))
+
+        # dataの長さが1000を超えている場合は、先頭から最新の1000個だけを使用する
+        if len(data[0]) > 10000:
+            data = data[:, -10000:]
+
+        # プロットアイテムごとに色を設定する
+        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0),
+                  (0, 255, 255), (255, 0, 255), (128, 0, 128), (255, 128, 0)]
+        for i, plot_data_item in enumerate(self.plot_data_items):
+            plot_data_item.setData(data[i, :])
+            plot_data_item.setPen(pg.mkPen(color=colors[i], width=2))
 
 
 if __name__ == '__main__':
