10810e309a70fafc66b6d0753bbdebb5463b7fe3
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-05-11 12:05:18 +0900

    add: change by time

--- host.py
+++ host.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from pylsl import StreamInfo, StreamOutlet
+from pylsl import StreamInlet, resolve_stream
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt, QTimer
 import pyqtgraph as pg
@@ -10,28 +10,27 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # ストリーム情報を作成しストリームアウトレットを作成
-        stream_info = StreamInfo('MyStream', 'Data', 2, 100, 'float32', 'myuid2424')
-        self.outlet = StreamOutlet(stream_info)
+        # ストリームを検索してStreamInletオブジェクトを作成
+        streams = resolve_stream('name', 'MyStream')
+        self.inlet = StreamInlet(streams[0])
 
         # グラフを作成
         self.plot_widget = pg.PlotWidget()
         self.setCentralWidget(self.plot_widget)
         self.plot_data_item = self.plot_widget.plot(pen='y')
 
-        # タイマーを設定して定期的にデータを送信する
+        # グラフの初期値を設定
+        self.plot_data_item.setData(np.zeros(8))
+
+        # タイマーを設定して定期的にデータを受信する
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.send_data)
-        self.timer.start(1000)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(50)
 
-    def send_data(self):
-        # 時間と sin 波の値を含んだデータを生成し送信
-        t = np.linspace(0, 1, 100)
-        data = np.vstack([t, np.sin(2 * np.pi * 10 * t)]).T
-        self.outlet.push_chunk(data.tolist())
-
-        # データをプロット
-        self.plot_data_item.setData(data[:, 0], data[:, 1])
+    def update_plot(self):
+        # データを取得してプロット
+        sample, timestamp = self.inlet.pull_sample()
+        self.plot_data_item.setData(np.hstack((self.plot_data_item.getData()[0][1:], sample)))
 
 
 if __name__ == '__main__':
@@ -39,4 +38,3 @@ if __name__ == '__main__':
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
-
